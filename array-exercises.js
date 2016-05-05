dd5b4d0d71eb3774e2a9aac4f740b2cb1d160289
Jesse Wang <jesse.wang19@gmail.com>
2016-05-04 21:51:42 -0400

    Added files via upload

---
+++ array-exercises.js
@@ -0,0 +1,91 @@
+/********************/
+/*  Array Exercises */
+/********************/
+
+/*
+  DO NOT EDIT/DELETE THESE FUNCTIONS. They are used for the exercises!
+*/
+function process(item) {
+  return item;
+}
+
+/*
+  EXAMPLE ANSWER
+  Create a function named 'example' with no parameters.
+  Return an array consisting of three values
+  ie:
+    example() -> an array with 3 items
+*/
+function example() {
+  return [1, 2, 3];
+}
+
+/*
+  Create a function named 'quadArray' with no parameters
+  Return an array consisting of four values
+  ie:
+    quadArray() -> an array with 4 items
+*/
+
+
+/*
+  Create a function named 'itemCount' with one parameter
+  Name the parameter anything you want
+  Return the number of items in the array passed in through the parameter
+  ie:
+    itemCount([1, 2, 3]) -> 3
+    itemCount(["hello", "hola", "bonjour", "ni hao"]) -> 4
+*/
+
+
+/*
+  Create a function named 'firstItem' with one parameter
+  Name the parameter anything you want
+  Return the first item in the array passed in through the parameter
+  ie:
+    itemCount([1, 2, 3]) -> 1
+    itemCount(["hello", "hola", "bonjour", "ni hao"]) -> "hello"
+*/
+
+
+/*
+  Create a function named 'lastItem' with one parameter
+  Name the parameter anything you want
+  Return the last item in the array passed in through the parameter
+  ie:
+    lastItem([1, 2, 3]) -> 3
+    lastItem(["hello", "hola", "bonjour", "ni hao"]) -> "ni hao"
+*/
+
+
+/*
+  Create a function named 'arrayEdge' with one parameter
+  Name the parameter anything you want
+  Return the first and last item in the array passed through the parameter as a new array
+  ie:
+    arrayEdge([1, 2, 3]) -> [1, 3]
+    arrayEdge(["hello", "hola", "bonjour", "ni hao"]) -> ["hello", "ni hao"]
+*/
+
+
+/*
+  Create a function named 'processArray' with one parameter
+  Name the parameter anything you want
+  Create a for loop that runs based on the length of the array entered as the parameter
+  Inside the for loop, call the process function with the argument for each item in the array
+  ie:
+    processArray([1, 2, 3]) -> process(1), process(2) and process(3) are called
+    processArray(["hello", "hola", "bonjour", "ni hao"]) -> process("hello"), process("hola"), process("bonjour") and process("ni hao") are called
+*/
+
+
+/*
+  Create a function named 'filterArray' with two parameters
+  Name the parameters anything you want
+  The first parameter will be an array, the second parameter will be a number
+  Return a new array that will consist of items that are greater than or equal to the second parameter
+  ie:
+    filterArray([1, 2, 3], 2) -> [2, 3]");
+    filterArray([7, 3, 10, 6], 5) -> [7, 10, 6]");
+    filterArray([7, 3, 10, 6], 20) -> []");
+*/
---
+++ for-loop-exercises.js
@@ -0,0 +1,86 @@
+/***********************/
+/*  For Loop Exercises */
+/***********************/
+
+/*
+  DO NOT EDIT/DELETE THESE FUNCTIONS. They are used for the exercises!
+*/
+function run() {}
+function stop() {}
+
+/*
+  EXAMPLE ANSWER
+  Create a function named 'example' with no parameters
+  Inside the body, create a for loop that repeats 3 times
+  Inside the for loop, call run()
+*/
+function example() {
+  for (var i = 0; i < 3; i++) {
+    run();
+  }
+}
+
+/*
+  Create a function named 'looper' with no parameters
+  Inside the body, create a for loop that repeats 5 times
+  Inside the for loop, call run()
+*/
+
+
+/*
+  Create a function named 'inception' with one parameter
+  Name the parameter anything you want
+  Inside the body, create a for loop that repeats based on the value of the parameter
+  Inside the for loop, call run()
+  ie:
+    inception(2) -> run() is called twice
+    inception(3) -> run() is called thrice
+    inception(4) -> run() is called four times
+    inception(0) -> run() should not be called
+*/
+
+
+/*
+  Create a function named 'redLightGreenLight' with two parameters
+  Name the parameters anything you want
+  Inside the body, create a for loop that repeats based on the value of the first parameter
+  If the second parameter is true, run() is called, else stop() is called
+  ie:
+    redLightGreenLight(2, true) -> run() is called twice
+    redLightGreenLight(3, false) -> stop() is called thrice
+    redLightGreenLight(4, true) -> run() is called four times
+    redLightGreenLight(0, false) -> stop() should not be called
+*/
+
+
+/*
+  Create a function named 'range' with two parameters
+  Name the parameters anything you want
+  Inside the body, create a for loop that repeats based on the two parameters
+  The first parameter will indicate the STARTING point
+  The second parameter will indicate the ENDING points
+  Inside the for loop, call run()
+  ie:
+    range(0, 2) -> run() is called twice
+    range(1, 2) -> run() is called once
+    range(3, 6) -> run() is called thrice
+    range(2, 0) -> run() should not be called
+*/
+
+
+/*
+  Create a function named 'rangeAdv' with three parameters
+  Name the parameters anything you want
+  Inside the body, create a for loop that repeats based on the three parameters
+  The first parameter will indicate the STARTING point
+  The second parameter will indicate the ENDING points
+  The third parameter will decide how much the loop will increment
+    (hint: you will need to change the i++ part)
+  Inside the for loop, call run()
+  ie:
+    rangeAdv(0, 2, 1) -> run() is called twice
+    rangeAdv(0, 4, 2) -> run() is called twice
+    rangeAdv(1, 5, 3) -> run() is called once
+    rangeAdv(2, 0, 5) -> run() should not be called
+    rangeAdv(0, 2, 5) -> run() is called once
+*/
